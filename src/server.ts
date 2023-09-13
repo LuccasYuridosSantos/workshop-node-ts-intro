41fd78d4d4011903f48851c5784bce2c1b7b2590
Luccas Yuri dos Santos <66194181+LuccasYuridosSantos@users.noreply.github.com>
2023-09-12 21:58:22 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import dataBase from './database/ormconfig'
-
 import routes from './routes'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://meuapp.com']
+})) // habilita o cors
 app.use(express.json()) // habilita o express para receber dados no formato json
 app.use(routes) // habilita as rotas
 
